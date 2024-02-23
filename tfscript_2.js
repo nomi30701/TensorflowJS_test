f7ddd20c1d5b7620843693086f69b5806993f958
Nomi <84728127+nomi30701@users.noreply.github.com>
2024-02-23 12:41:08 +0800

    add camera mode

--- tfscript_2.js
+++ tfscript_2.js
@@ -5,10 +5,8 @@ const classifier_btn = document.getElementById('classifier_btn');
 const mobilenet_state = document.getElementById('mobilenet_state');
 
 
-// Load model bnt
-async function load_mobilenet() {
-    classifier_btn.disabled = true; // disable btn
-    
+// Load model btn
+async function load_mobilenet() {    
     mobilenet_state.textContent = 'Loading mobilenet..';  // show state
     mobilenet_state.style.color = 'red';
     net = await mobilenet.load(); 
@@ -18,25 +16,105 @@ async function load_mobilenet() {
     classifier_btn.disabled = false; // enable btn
 }
 
+
+
 // Predict btn
+// async function app() {
+//     show_result.innerHTML = ''; // clean text
+
+//     if (isCameraActive) {
+//         const webcam = await tf.data.webcam(webcamElement);
+//         while (true) {
+//             const img = await webcam.capture();
+//             const result = await net.classify(img);
+            
+//             let htmlContent = '';
+//             result.forEach((obj, _) => {
+//                 const className = obj.className;
+//                 const probability = (obj.probability * 100).toFixed(2);
+//                 const firstClassName = className.split(',')[0].trim();
+//                 htmlContent += `
+//                 <p>Class: <span style="color: blue">${firstClassName}</span> | Prob: <span style="color: red">${probability}%</span></p>
+//                 `;
+//             });
+//             show_result.innerHTML = htmlContent;
+
+//             img.dispose(); //  釋放圖像內存
+//             await tf.nextFrame(); // 等待下一個 frame
+//         }
+//     }
+//     else{
+//         // Make a prediction through the model on our image.
+//         const imgEl = document.getElementById('img');
+//         const result = await net.classify(imgEl);
+
+//         // show to page
+//         let htmlContent = '';
+//         result.forEach((obj, _) => {
+//             const className = obj.className;
+//             const probability = (obj.probability * 100).toFixed(2);
+//             const firstClassName = className.split(',')[0].trim();
+//             htmlContent += `
+//             <p>Class: <span style="color: blue">${firstClassName}</span> | Prob: <span style="color: red">${probability}%</span></p>
+//             `;
+//         });
+//         show_result.innerHTML = htmlContent;
+    
+//         show_result.classList.add('Info_container');
+//         classifier.appendChild(show_result);
+//     }
+// }
+
 async function app() {
     show_result.innerHTML = ''; // clean text
 
-    // Make a prediction through the model on our image.
-    const imgEl = document.getElementById('img');
-    const result = await net.classify(imgEl);
-    
-    // show to page
-    result.forEach((obj, index) => {
-        const className = obj.className;
-        const probability = (obj.probability * 100).toFixed(2);
-        const firstClassName = className.split(',')[0].trim();
-        show_result.innerHTML += `
-        <p>Class: <span style="color: blue">${firstClassName}</span> | Prob: <span style="color: red">${probability}%</span></p>
+    const generateHtmlContent = (result) => {
+        let htmlContent = '';
+        result.forEach((obj) => {
+            const className = obj.className;
+            const probability = (obj.probability * 100).toFixed(2);
+            const firstClassName = className.split(',')[0].trim();
+            htmlContent += `
+            <p>Class: <span style="color: blue">${firstClassName}</span> | Prob: <span style="color: red">${probability}%</span></p>
+            `;
+        });
+        return htmlContent;
+    }
+
+    if (isCameraActive) {
+        const webcam = await tf.data.webcam(webcamElement);
+        while (isCameraActive) {
+            const img = await webcam.capture();
+            const result = await net.classify(img);
+            show_result.innerHTML = generateHtmlContent(result);
+            img.dispose(); //  Release image memory
+            await tf.nextFrame(); // Wait for the next frame
+        }
+        webcam.stop(); // Stop the webcam when not in use
+    }
+    else {
+        const imgEl = document.getElementById('img');
+        const result = await net.classify(imgEl);
+        show_result.innerHTML = generateHtmlContent(result);
+        show_result.classList.add('Info_container');
+        classifier.appendChild(show_result);
+    }
+}
+
+// media toggle
+var isCameraActive = false;
+function toggleCameraMode() {
+    if (isCameraActive) {
+        // If the camera is active, switch back to file input and image
+        document.getElementById('media-container').innerHTML = `
+            <input type="file" id="file" accept="image/jpeg, image/png"><br>
+            <img id="img" src="./JlUvsxa.jpg" width="227" height="227">
         `;
-    });
-    show_result.classList.add('Info_container');
-    classifier.appendChild(show_result);
+        isCameraActive = false;
+    } else {
+        document.getElementById('media-container').innerHTML = '<video autoplay playsinline muted id="webcam" width="224" height="224"></video>';
+        isCameraActive = true;
+    }
 }
 
 // if import img file
@@ -58,4 +136,3 @@ document.getElementById('file').addEventListener('change', function(event) {
         reader.readAsDataURL(file); // 讀取文件並轉換為 DataURL
     }
 });
-
